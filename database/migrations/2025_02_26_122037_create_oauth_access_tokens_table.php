5c84c64ee5fe6f357e07705d77deaf84335b948e
Omar Elmahi <Oelmahi31@gmail.com>
2025-02-26 12:28:35 +0000

    Update Migrations

--- database/migrations/2025_02_26_122037_create_oauth_access_tokens_table.php
+++ database/migrations/2025_02_26_122037_create_oauth_access_tokens_table.php
@@ -12,14 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('oauth_access_tokens', function (Blueprint $table) {
-            $table->string('id', 100)->primary();
-            $table->unsignedBigInteger('user_id')->nullable()->index();
-            $table->unsignedBigInteger('client_id');
-            $table->string('name')->nullable();
-            $table->text('scopes')->nullable();
-            $table->boolean('revoked');
+            $table->id();
             $table->timestamps();
-            $table->dateTime('expires_at')->nullable();
         });
     }
 
